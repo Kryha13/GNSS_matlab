cec69aeaada115a1348140912169205235644089
Kryha13 <krystian.qaz123@gmail.com>
2019-03-03 18:54:38 +0100

    read bias file

--- readBIA.m
+++ readBIA.m
@@ -1,4 +1,4 @@
-function [biases] = readBIA(fileTime, sysConst)
+function [bias] = readBIA(fileTime, sysConst)
 
 const
 filePath = ['gbm', num2str(fileTime(1)), num2str(fileTime(2)), '.bia'];
@@ -6,7 +6,7 @@ filePath = ['gbm', num2str(fileTime(1)), num2str(fileTime(2)), '.bia'];
 disp(['Reading BIA file : ',filePath]);
 idFile  = fopen (filePath);
 
-bias = [];
+bias = {};
 nObs = 1;
 
 while 1
@@ -19,15 +19,15 @@ while 1
                     if (strfind(line(7), sysConst))
 
                         constellation = cellstr(splitLine(3));
-                        prn = splitLine(4);
+                        prn = cellstr(splitLine(4));
 
                         station = splitLine(5);
                         obs1 = splitLine(6);
                         obs2 = splitLine(7);
-                        bias_value = splitLine(11);
-                        std = splitLine(12);
+                        bias_value = str2doubleq(splitLine(11));
+                        std = str2doubleq(splitLine(12));
 
-                        bias(nObs,:) = [constellation, prn, station, obs1, obs2, bias_value, std];
+                        bias(nObs,:) = {constellation, prn, station, obs1, obs2, bias_value, std};
                         nObs= nObs+1;
 
                     end
