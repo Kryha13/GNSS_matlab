979071526901fc276a4abb078ef1db0768a3918d
Kryha13 <krystian.qaz123@gmail.com>
2019-03-18 20:05:35 +0100

    single differences for approx pseudoranges

---
+++ single_diff_range.m
@@ -0,0 +1,34 @@
+function [single_diff] = single_diff_range(time_interval, ro_approx1, ro_approx2, Az_EL_R, data)
+load(data)
+% [sysPrefix, sysName] = sysGNSS(system); % dla Galileo 
+epochs = gpsSecondsFirst:time_interval:gpsSecondsLast;
+single_diff = {};
+j = 1;
+    for i=1:length(epochs)
+    
+        act_constellation_1 = squeeze(ro_approx1(:,2,i)); 
+        act_constellation_2 = squeeze(ro_approx2(:,2,i));
+        act_constellation = intersect(act_constellation_1, act_constellation_2, 'stable');
+        nsat=length(act_constellation);
+        [sat_ref, sat_idx] = find_ref_sat(Az_EL_R, i); % satelita referencyjny
+        % tu moze byc problem jak cos 
+        sat_ref = find(act_constellation==sat_ref); % indeks w aktualnej konstelacji
+        
+        for k=1:nsat
+                
+            prn_num = act_constellation(k); % numer k-ty satelita w danej epoce
+            idx_1 = find(squeeze(ro_approx1(:,2,i))==prn_num);
+            idx_2 = find(squeeze(ro_approx2(:,2,i))==prn_num);
+
+            single_diff(k,i) = {ro_approx1(idx_1, 3, i) - ro_approx2(idx_2, 3, i)};
+%                 single_diff(single_diff==0) = NaN;
+            % satelita referencyjny jako pierwszy
+            if k==nsat
+                single_diff([1 sat_ref],i) = single_diff([sat_ref 1],i);
+            end
+        end
+        
+    end
+
+end
+
