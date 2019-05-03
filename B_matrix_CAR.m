19cd06eef22841ca6db0941301f8899c82e057d2
Kryha13 <krystian.qaz123@gmail.com>
2019-05-03 22:01:09 +0200

    car method algorithm complete

--- B_matrix_CAR.m
+++ B_matrix_CAR.m
@@ -1,15 +1,15 @@
 function [B] = B_matrix_CAR(step_number, obs_qty)
 
 const;
-phase_freq = [fE1 fE6 fE5 fE7];
+phase_freq = [fE1 fE6 fE7 fE5];
 lam_up = c/fE1 * c/fE6 * c/fE5 * c/fE7;
-lam_down = [c/fE6*c/fE5*c/fE7 c/fE1*c/fE5*c/fE7 c/fE1*c/fE6*c/fE7 c/fE1*c/fE6*c/fE5];
+lam_down = [c/fE6*c/fE5*c/fE7 c/fE1*c/fE5*c/fE7 c/fE1*c/fE6*c/fE5 c/fE1*c/fE6*c/fE7];
 com1 = [0; 0; 1; -1];
 com2 = [0; 1; -1; 0];
 com3 = [1; -1; 0; 0];
 com4 = [0; 0; 0; 1];
 D = [com1; com2; com3; com4];
-phase_obs_qty = obs_qty(1:length(obs_qty)/2, 2);
+phase_obs_qty = obs_qty(:,2);
 
     switch step_number        
         case 1
@@ -34,8 +34,8 @@ phase_obs_qty = obs_qty(1:length(obs_qty)/2, 2);
                B4 = eye(str2double(phase_obs_qty(4)))*(c/phase_freq(4));
                B_L = [Bcom2 Bcom3 B4; Bcom2 zeros(size(Bcom2)) B4; ...
                    zeros(size(Bcom2)) zeros(size(Bcom2)) B4];
-               B0 = zeros(str2double(phase_obs_qty(1)));
-               B = [B_L; B0];
+               B0 = zeros(str2double(phase_obs_qty(1)), size(B_L,2));
+               B = [B0; B_L];
            else
                B = [];
            end
@@ -45,15 +45,15 @@ phase_obs_qty = obs_qty(1:length(obs_qty)/2, 2);
                Bcom3 = eye(str2double(phase_obs_qty(1)))*((lam_up/sum(lam_down*com3)));
                B4 = eye(str2double(phase_obs_qty(4)))*(c/phase_freq(4));
                B_L = [Bcom3 B4; zeros(size(Bcom3)) B4; zeros(size(Bcom3)) B4];
-               B0 = zeros(str2double(phase_obs_qty(1)));
-               B = [B_L; B0];
+               B0 = zeros(str2double(phase_obs_qty(1)), size(B_L,2));
+               B = [B0; B_L];
             end
             
         case 4
             B4 = eye(str2double(phase_obs_qty(4)))*(c/phase_freq(4));
             B_L = [B4; B4; B4];
-            B0 = zeros(size(B_L));
-            B = [B_L; B0];
+            B0 = zeros(str2double(phase_obs_qty(1)), size(B_L,2));
+            B = [B0; B_L];
         
     end
 end
--- get_com_lambda.m
+++ get_com_lambda.m
@@ -1,13 +1,15 @@
 function [com_lam] = get_com_lambda(combination)
     
 const;
-phase_freq = [fE1 fE6 fE5 fE7];
+phase_freq = [fE1 fE6 fE7 fE5];
 lam_up = c/fE1 * c/fE6 * c/fE5 * c/fE7;
-lam_down = [c/fE6*c/fE5*c/fE7 c/fE1*c/fE5*c/fE7 c/fE1*c/fE6*c/fE7 c/fE1*c/fE6*c/fE5];
+lam_down = [c/fE6*c/fE5*c/fE7 c/fE1*c/fE5*c/fE7 c/fE1*c/fE6*c/fE5 c/fE1*c/fE6*c/fE7];
 com1 = [0; 0; 1; -1];
 com2 = [0; 1; -1; 0];
 com3 = [1; -1; 0; 0];
 com4 = [0; 0; 0; 1];
+trans1 = [0; 5; -1; -4];
+trans2 = [5; 0; -2; -3];
 
     switch combination       
         case 1
@@ -16,6 +18,10 @@ com4 = [0; 0; 0; 1];
             com_lam = abs((lam_up/sum(lam_down*com2)));
         case 3 
             com_lam = abs((lam_up/sum(lam_down*com3)));
+        case 4
+            com_lam = abs((lam_up/sum(lam_down*trans1)));
+        case 5
+            com_lam = abs((lam_up/sum(lam_down*trans2)));
     end
 end
 
